f0418e151bc148b9e2119a1d4886e7944f0349b9
Nguyen VanFalk <nvanfalk@gmail.com>
2019-01-03 00:04:04 -0800

    initial Push and commit for Scriptable repo
    Adding synced directory

---
+++ Key.js
@@ -0,0 +1,42 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: green; icon-glyph: thumbs-down;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-brown; icon-glyph: magic-wand;
+let html = `
+<style>
+body {
+  margin: 0;
+  padding 0;
+  background: black;
+}
+.vid {
+  width: 100%;
+  height: 100%;
+}
+</style>
+<div id="player"></div>
+<script>
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'UThGcWBIMpU',
+    events: {
+    'onReady': onPlayerReady
+  }
+  });
+}
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.seekTo(1923, true);
+}
+</script>
+`
+WebView.loadHTML(html, null, new Size(0, 202))
